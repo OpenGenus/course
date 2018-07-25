97ac4a23f10927194d857957f7b39736bc3eac9d
davidxuan <40924825+davidxuan@users.noreply.github.com>
2018-07-24 23:50:49 -0700

    Update settings.py

--- course/settings.py
+++ course/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
